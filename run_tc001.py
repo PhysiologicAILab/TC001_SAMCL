2a2e23cd55d5c7ec6fe49f37ec5f842820fb008c
jnj256 <jnj256@gmail.com>
2024-01-29 09:50:43 +0000

    trying ffmpeg pipeline for data acquisition

---
+++ run_tc001.py
@@ -0,0 +1,144 @@
+import cv2
+import subprocess
+import os
+import platform
+import numpy as np
+from typing import Tuple
+import matplotlib.pyplot as plt
+
+class VideoStreamFFmpeg:
+    def __init__(self, src: int, fps: int, resolution: Tuple[int, int]):
+        self.src = src
+        self.fps = fps
+        self.resolution = resolution
+        self.pipe = self._open_ffmpeg()
+        self.frame_shape = (self.resolution[1], self.resolution[0], 2)
+        self.frame_size = np.prod(self.frame_shape)
+        self.wait_for_cam()
+
+    def _open_ffmpeg(self):
+        os_name = platform.system()
+        if os_name == "Darwin":  # macOS
+            input_format = "avfoundation"
+            video_device = f"{self.src}:none"
+        elif os_name == "Linux":
+            input_format = "v4l2"
+            video_device = f"{self.src}"
+        elif os_name == "Windows":
+            input_format = "dshow"
+            video_device = f"video={self.src}"
+        else:
+            raise ValueError("Unsupported OS")
+
+        command = [
+            'ffmpeg',
+            '-f', input_format,
+            '-r', str(self.fps),
+            '-video_size', f'{self.resolution[0]}x{self.resolution[1]}',
+            '-i', video_device,
+            '-vcodec', 'rawvideo',  # 'mjpeg',  # Input codec set to mjpeg
+            '-an', '-vcodec', 'rawvideo',  # Decode the MJPEG stream to raw video
+            '-pix_fmt', 'bgra',  # 'bgr24',
+            '-vsync', '2',
+            '-f', 'image2pipe', '-'
+        ]
+
+        if os_name == "Linux":
+            command.insert(2, "-input_format")
+            command.insert(3, "mjpeg")
+
+        return subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=10**8
+        )
+
+    def read(self):
+        raw_image = self.pipe.stdout.read(self.frame_size)
+        if len(raw_image) != self.frame_size:
+            return None
+        # image = np.frombuffer(raw_image, dtype=np.uint8).reshape(self.frame_shape)
+        image = np.frombuffer(raw_image, dtype=np.uint8).reshape(self.frame_shape)
+        return image
+
+    def release(self):
+        self.pipe.terminate()
+
+    def wait_for_cam(self):
+        for _ in range(30):
+            frame = self.read()
+        if frame is not None:
+            return True
+        return False
+
+
+class VideoStreamCV:
+    def __init__(self, src: int, fps: int, resolution: Tuple[int, int]):
+        self.src = src
+        self.fps = fps
+        self.resolution = resolution
+        self.cap = self._open_camera()
+        self.wait_for_cam()
+
+    def _open_camera(self):
+        cap = cv2.VideoCapture(self.src)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.resolution[0])
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.resolution[1])
+        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+        cap.set(cv2.CAP_PROP_FOURCC, fourcc)
+        cap.set(cv2.CAP_PROP_FPS, self.fps)
+        return cap
+
+    def read(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            return None
+        return frame
+
+    def release(self):
+        self.cap.release()
+
+    def wait_for_cam(self):
+        for _ in range(30):
+            frame = self.read()
+        if frame is not None:
+            return True
+        return False
+
+
+
+src = 0
+fps = 25
+resolution = (256, 192)
+
+cam = VideoStreamFFmpeg(src=0, fps=fps, resolution=resolution)
+# cam = VideoStreamCV(src=0, fps=fps, resolution=resolution)
+
+count = 0
+
+while (True):
+    # inside infinity loop
+    frame = cam.read()
+    if count == 0:
+        count += 1
+        print("Frame shape:", frame.shape)
+
+    # _, thdata = np.array_split(frame, 2)
+    hi = frame[..., 0]
+    lo = frame[..., 1]
+    lo = lo * 256.0
+    raw_temp = hi + lo
+    raw_temp = (raw_temp / 64.0) - 273.15
+
+    # plt.imshow(frame[..., 0])
+    plt.imshow(raw_temp)
+    plt.show()
+    exit()
+    # cv2.imshow('frame', raw_temp)
+
+    # print(ret)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cam.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
--- tc001_live_seg.py
+++ tc001_live_seg.py
@@ -1,13 +1,34 @@
 import cv2
 import numpy as np
 import argparse
-from inference import ThermSeg
+# from inference import ThermSeg
 from utils.configer import Configer
 import os
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import sys
 
+vid = cv2.VideoCapture(0)
+vid.set(3, 256)
+vid.set(4, 192)
+# vid.set(cv2.CAP_PROP_CONVERT_RGB, 0)
+# vid.set(10, 150)
+
+while (True):
+    # inside infinity loop
+    ret, frame = vid.read()
+    if ret:
+        cv2.imshow('frame', frame)
+    # print(ret)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+
+'''
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 
@@ -46,41 +67,62 @@ if __name__ == "__main__":
 
     width = 256  # Sensor width
     height = 192  # sensor height
-
+    nCh = 3 #1
 
     if 'linux' in sys.platform.lower():
         cap = cv2.VideoCapture('/dev/video'+str(args_parser.device))
     elif 'win' in  sys.platform.lower():
         cap = cv2.VideoCapture(int(args_parser.device), cv2.CAP_DSHOW)
     else:
-        cap = cv2.VideoCapture(int(args_parser.device))
+        cap_device = -1 # built-in camera of MacBook Pro 2018
+        cap = cv2.VideoCapture(cap_device)	
+        cap.set(cv2.CAP_PROP_FPS, 60) # newly added code
+        cfps = int(cap.get(cv2.CAP_PROP_FPS))
+        # cap = cv2.VideoCapture(2)
+        # cap.release()
+        # cap = cv2.VideoCapture(2)
 
-    cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
-    cap.set(3, width)
-    cap.set(4, height)
+        # cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+
+    # cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
+    # cap.set(3, width)
+    # cap.set(4, height)
     # cap.set(10, 150)
 
     configer = Configer(args_parser=args_parser)
     ckpt_root = configer.get('checkpoints', 'checkpoints_dir')
     ckpt_name = configer.get('checkpoints', 'checkpoints_name')
     configer.update(['network', 'resume'], os.path.join(ckpt_root, ckpt_name + '.pth'))
-    segObj = ThermSeg(configer)
-    segObj.load_model(configer)
+    # segObj = ThermSeg(configer)
+    # segObj.load_model(configer)
 
     # First set up the figure, the axis, and the plot element we want to animate
     fig, ax = plt.subplots(figsize=(8,8))
-    init_image1 = (np.random.rand(height, width) + 1) * 15
-    init_image2 = (np.random.rand(height, width) + 1) * 3
-    im1 = ax.imshow(init_image1, cmap='gray', vmin=10, vmax=40)
-    im2 = ax.imshow(init_image2, cmap='seismic', alpha=0.5, vmin=0, vmax=6)
+
+    if nCh == 1:
+        init_image1 = (np.random.rand(height, width) + 1) * 15
+    else:
+        # init_image1 = np.round((np.random.rand(height, width, nCh) + 1) * 255).astype(np.uint8)
+        while True:
+            # Capture frame-by-frame
+            ret, init_image1 = cap.read()
+            if ret:
+                break
+
+
+    # im1 = ax.imshow(init_image1, cmap='gray', vmin=10, vmax=40)
+    im1 = ax.imshow(init_image1)
+
+    # init_image2 = (np.random.rand(height, width) + 1) * 3
+    # im2 = ax.imshow(init_image2, cmap='seismic', alpha=0.5, vmin=0, vmax=6)
 
     # fps = 25
     # nSeconds = 10
 
     def init():
         ax.set_axis_off()
-        return im1, im2
-
+        # return im1, im2
+        return im1,
 
     def live_capture(i):
 
@@ -88,22 +130,31 @@ if __name__ == "__main__":
             # Capture frame-by-frame
             ret, frame = cap.read()
             if ret == True:
-                _, thdata = np.array_split(frame, 2)
-                hi = thdata[..., 0]
-                lo = thdata[..., 1]
-                lo = lo * 256.0
-                raw_temp = hi + lo
-                raw_temp = (raw_temp / 64.0) - 273.15
+                im1.set_array(frame)
+        return im1,
 
-                pred_seg_mask, time_taken = segObj.run_inference(raw_temp)
+    # def live_capture(i):
 
-                im1.set_array(raw_temp)
-                im2.set_array(pred_seg_mask)
-                # plt.imshow(raw_temp, cmap='gray')
-                # plt.imshow(pred_seg_mask, cmap='seismic', alpha=0.5)
-                # plt.show()
+    #     if (cap.isOpened()):
+    #         # Capture frame-by-frame
+    #         ret, frame = cap.read()
+    #         if ret == True:
+    #             _, thdata = np.array_split(frame, 2)
+    #             hi = thdata[..., 0]
+    #             lo = thdata[..., 1]
+    #             lo = lo * 256.0
+    #             raw_temp = hi + lo
+    #             raw_temp = (raw_temp / 64.0) - 273.15
 
-        return im1, im2
+    #             pred_seg_mask, time_taken = segObj.run_inference(raw_temp)
+
+    #             im1.set_array(raw_temp)
+    #             im2.set_array(pred_seg_mask)
+    #             # plt.imshow(raw_temp, cmap='gray')
+    #             # plt.imshow(pred_seg_mask, cmap='seismic', alpha=0.5)
+    #             # plt.show()
+
+    #     return im1, im2
 
 
     ani = FuncAnimation(
@@ -119,3 +170,4 @@ if __name__ == "__main__":
 
 
 
+'''
